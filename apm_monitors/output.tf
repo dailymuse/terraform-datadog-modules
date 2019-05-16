06fd5b4f08cc0e9b5cc21845dec5cd3adf017d90
Dan Cadden <Shikkic@gmail.com>
2019-05-16 14:51:01 -0400

    add monitor id outputs

--- apm_monitors/output.tf
+++ apm_monitors/output.tf
@@ -1,15 +1,15 @@
 output "high_average_latency_apm_monitor_id" {
-  value = "${datadog_monitor.high_average_latency.*.id}"
-}
-
-output "high_p90_latency_apm_monitor_id" {
-  value = "${datadog_monitor.high_p90_latency.*.id}"
+  value = "${module.high_average_latency.apm_monitor_id}"
 }
 
 output "high_error_rate_apm_monitor_id" {
-  value = "${datadog_monitor.high_error_rate.*.id}"
+  value = "${module.high_error_rate.apm_monitor_id}"
+}
+
+output "high_p90_latency_apm_monitor_id" {
+  value = "${module.high_p90_latency.apm_monitor_id}"
 }
 
 output "abnormal_throughput_apm_monitor_id" {
-  value = "${datadog_monitor.abnormal_throughput.*.id}"
+  value = "${module.abnormal_throughput.apm_monitor_id}"
 }
