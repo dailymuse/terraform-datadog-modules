6cf6a393d8d51c6a7b9c2dc179d64d908c1332af
Dan Cadden <Shikkic@gmail.com>
2019-05-20 10:11:51 -0400

    update variable descriptions

--- apm_monitor/variables.tf
+++ apm_monitor/variables.tf
@@ -1,48 +1,49 @@
-variable "env" {}
+variable "alert_message_output" {
+  description = "The '@username' notifications where you want alert messages to go."
+}
 
-variable "service_name" {
-  description = "The service name of that is being monitored."
+variable "create" {
+  description = "(Boolean) Whether an alarm should be created or not."
+}
+
+variable "env" {
+  description = "The environment the monitor is checking."
+}
+
+variable "escalation_message" {
+  description = "An optional message to include with a re-notification. Supports the '@username' notification allowed elsewhere."
+  default     = ""
 }
 
 variable "monitor_condition_name" {
-  description = "TODO"
+  description = "A short description of the alert condition."
 }
 
 variable "query" {
   description = "Query that the monitor will use to measure against."
 }
 
-variable "thresholds" {
-  type        = "map"
-  description = "TODO"
-}
-
-variable "threshold_windows" {
-  type        = "map"
-  description = "TODO"
-
-  default = {}
-}
-
-variable "escalation_message" {
-  description = "TODO"
-  default     = ""
+variable "service_name" {
+  description = "The service name that is being monitored."
 }
 
 variable "tags" {
   type        = "list"
-  description = "TODO"
+  description = "A list of tags to associate with your monitor. This can help you categorize and filter monitors in the manage monitors page of the UI. Note: it's not currently possible to filter by these tags when querying via the API"
   default     = []
 }
 
-variable "create" {
-  description = "TODO"
+variable "threshold_windows" {
+  type        = "map"
+  description = "A mapping containing recovery_window and trigger_window values, e.g. last_15m. Can only be used for anomaly monitors."
+  default     = {}
 }
 
-variable "alert_message_output" {
-  description = "TODO"
+variable "thresholds" {
+  type        = "map"
+  description = "A dictionary of thresholds by threshold type. Currently we have four threshold types for metric alerts: critical, critical recovery, warning, and warning recovery"
 }
 
 variable "type" {
-  description = "TODO"
+  description = "The type of the monitor"
 }
